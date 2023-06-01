ac22ad2d917d45a5c6803ca7fb350df954ae05e0
Sanjeet Rai <raisanjeet8896@gmail.com>
2023-06-01 18:12:24 +0530

    stablized view

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const btns = document.querySelectorAll(".question-btn");
 const quest = document.querySelectorAll(".question-title");
+
 btns.forEach(function(btn,index){
     btn.addEventListener('click', function(e){
-         quest[index].childNodes[1].classList.toggle("toggle-font");
+        checkActive();
+        quest[index].childNodes[1].classList.toggle("toggle-font");
         const question = e.currentTarget.parentElement.parentElement;
         question.classList.toggle("show-text");
-        // document.body.quest.fontWeight="bold";
+        
     });
 });
 
+function checkActive(){
+    btns.forEach(function(btn,index){
+        const activeBtn=btn.parentElement.parentElement;
+        if(activeBtn.classList.contains("show-text")){
+         quest[index].childNodes[1].classList.toggle("toggle-font");
+            activeBtn.classList.remove("show-text");
+        }
+        });
+}
