1f4fa7e2ef59c7fb911b2bca2423775c93dc97d0
Sanjeet Rai <raisanjeet8896@gmail.com>
2023-06-03 11:38:37 +0530

    clicking on questions features included

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 const btns = document.querySelectorAll(".question-btn");
 const quest = document.querySelectorAll(".question-title");
-const articles = document.querySelectorAll(".questions");
+
 let currentIndex=0;
 
+quest.forEach(function(faq,index){
+    faq.addEventListener('click', function(event){
+        if(index!==currentIndex)
+        {
+        checkActive();
+        }
+        quest[index].childNodes[1].classList.toggle("toggle-font");
+        const selectParent = event.currentTarget.parentElement;
+        selectParent.classList.toggle("show-text");
+        currentIndex=index;
+
+
+    });
+});
+
+
+
+// using buttons you can also perform the same task
+function checkButtons(){
 btns.forEach(function(btn,index){
     btn.addEventListener('click', function(e){
         if(index!==currentIndex)
@@ -12,11 +31,14 @@ btns.forEach(function(btn,index){
         
         quest[index].childNodes[1].classList.toggle("toggle-font");
         const question = e.currentTarget.parentElement.parentElement;
+        console.log(question);
         question.classList.toggle("show-text");
         currentIndex=index;
         
     });
 });
+}
+
 
 function checkActive(){
     btns.forEach(function(btn,index){
