c7dab4adb886e194364ccc0755dfb9a0ee01e422
Sanjeet Rai <raisanjeet8896@gmail.com>
2023-06-03 10:58:56 +0530

    FAQ project completed

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const btns = document.querySelectorAll(".question-btn");
 const quest = document.querySelectorAll(".question-title");
+const articles = document.querySelectorAll(".questions");
+let currentIndex=0;
 
 btns.forEach(function(btn,index){
     btn.addEventListener('click', function(e){
+        if(index!==currentIndex)
+        {
         checkActive();
+        }
+        
         quest[index].childNodes[1].classList.toggle("toggle-font");
         const question = e.currentTarget.parentElement.parentElement;
         question.classList.toggle("show-text");
+        currentIndex=index;
         
     });
 });
